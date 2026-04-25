f337eaf604224b7da069741200fafff42df64fb5
snowykte0426 <snowykte0426@naver.com>
2026-04-25 17:02:05 +0900

    fix(network): 연결 실패 화면 개선 및 카운트다운 정확도 수정

--- composeApp/src/commonMain/kotlin/com/cowork/app_client/data/network/ConnectivityMonitor.kt
+++ composeApp/src/commonMain/kotlin/com/cowork/app_client/data/network/ConnectivityMonitor.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
